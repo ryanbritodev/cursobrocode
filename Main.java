e2229ff4aee58e7750b82f4393ec5b6c96578b93
Ryan Brito <rm554497@fiap.com.br>
2024-11-26 08:08:28 -0300

    ☕  add: Primeiro projeto em Java

--- Main.java
+++
@@ -1,8 +0,0 @@
-// Classe Principal do Programa
-public class Main {
-    // Metodo principal da classe
-    // Se retirarmos ele, ele não compila e pede para criarmos nosso metodo principal
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
